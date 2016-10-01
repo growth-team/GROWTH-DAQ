1dcf435ff0050bd45f2347b4281a94735c87a00f
Takayuki Yuasa <yuasatakayuki@gmail.com>
2016-10-01 20:04:32 +0800

    growth_file_relocator.rb - compression implemented

--- controller/bin/growth_file_relocator.rb
+++ controller/bin/growth_file_relocator.rb
@@ -8,6 +8,8 @@ require "fileutils"
 # Relocates DAQ/HK Logger output files into
 # appropriate subfolders (e.g. 201610/ or 201610/hk for
 # DAQ output FITS files and HK Logger output HK files).
+# If a file is not gzip-compressed, this program automatically
+# compresses it when moving to the subfolder.
 
 class FileRelocator
 
@@ -16,17 +18,18 @@ class FileRelocator
 
   def initialize()
     @logger = GROWTH.logger(ARGV, "growth_file_relocator")
+    @logger.level = Logger::DEBUG
     @growth_config = GROWTH::Config.new(logger: @logger)
     # Instantiate subsystems
     @daq = GROWTH::ConsoleModuleDAQ.new("daq", logger: @logger)
     @hk = GROWTH::ConsoleModuleHK.new("hk", logger: @logger)
     # Construct root dir to work in
-    @root_dir = GROWTH_DATA_DIR + @growth_config.detector_id
+    @root_dir = GROWTH_DATA_DIR + "/"+ @growth_config.detector_id
     @logger.info("Started")
     @logger.info("Root dir: #{@root_dir}")
     @logger.info("Relocation period: #{RELOCATION_PERIOD} sec")
     if !Dir.exist?(@root_dir) then
-      @logger.fatal("Rood dir not found. Exiting...")
+      @logger.fatal("Root dir not found. Exiting...")
       exit 1
     end
     Dir.chdir(@root_dir)
@@ -41,23 +44,36 @@ class FileRelocator
         daq_current_output_file_name = daq_status["outputFileName"]
       end
 
-      @logger.debug("Checking files need to be relocated")
-      daq_output_files = Dir.glob("2*.fits")
-      hk_logger_output_files = Dir.glob("hk_2*").sort().pop
+      @logger.info("Checking files need to be relocated")
+      daq_output_files = Dir.glob("2*.fits").sort()
+      hk_logger_output_files = Dir.glob("hk_2*").sort()
+      @logger.info("#{daq_output_files.length} DAQ output file(s) and #{hk_logger_output_files.length} HK file(s) detected")
+      # Remove the last DAQ file if communication with DAQ program was not successful
+      # because the failure may be due to non-existent growth_controller and DAQ program
+      # may be writing to this file.
+      if(daq_status["status"]!="ok")then
+        last_daq_output_file = daq_output_files.pop
+        @logger.warn("Communication with DAQ program failed.")
+      	@logger.warn("Skipping the latest DAQ output file #{last_daq_output_file} (this may be the current output of the DAQ program")
+      end
+      # Remove the last hk file because this may be the current output file
+      # of HK Logger
+      last_hk_logger_output_file = hk_logger_output_files.pop
+      @logger.info("Skipping the latest HK output file #{last_hk_logger_output_file} (may be current output of HK Logger)")
       files = []
       files.concat(daq_output_files)
       files.concat(hk_logger_output_files)
       files.each(){ |file_name|
         if daq_current_output_file_name.include?(file_name.strip) then
           @logger.info("Skipping #{file_name} (current output file of the DAQ program)")
-          continue
+          next
         end
         yyyymm = extract_yyyymm(file_name)
         destination_dir = yyyymm
         if file_name.include?("hk_") then
           destination_dir += "/hk"
         end
-        @logger.debug("Processing #{file_name} (destination #{destination_dir}")
+        @logger.info("Processing #{file_name} (destination #{destination_dir})")
         # Create destination folder if it does not exist
         if !Dir.exist?(destination_dir) then
           @logger.info("Creating #{destination_dir}")
@@ -70,12 +86,31 @@ class FileRelocator
         end
         # Move to the destination directory
         begin
-          FileUtils.mv(file_name, destination_dir)
+          if(!file_name.include?(".gz"))then
+            gz_file_name = file_name+".gz"
+            @logger.info("Compressing #{file_name}")
+            original_file_size_kb = File.size(file_name) / 1024.0
+            `gzip #{file_name}`
+            if(File.exist?(gz_file_name))then
+              gz_file_size_kb = File.size(gz_file_name) / 1024.0
+              ratio_percent = gz_file_size_kb / original_file_size_kb * 100
+              @logger.info(
+                "Successfully compressed from %.1fkB to %.1fkB (%.1f%%)" % [
+                 original_file_size_kb, gz_file_size_kb, ratio_percent
+              ])
+              # Now, moved file should end ".gz" file.
+              file_name = gz_file_name
+            else
+              @logger.warn("Compression failed for #{file_name}")
+            end
+            FileUtils.mv(file_name, destination_dir)
+          end
         rescue => e
           @logger.warn("Failed to move #{file_name} to #{destination_dir} (#{e}). Continuing...")
         end
       }
       # Wait for a while
+      @logger.info("Sleeping #{RELOCATION_PERIOD} sec")
       sleep(RELOCATION_PERIOD)
     end
   end
@@ -84,9 +119,10 @@ class FileRelocator
   def extract_yyyymm(file_name)
     file_name = File.basename(file_name)
     # Cut hk_ prefix (if exists)
-    file_name.gsub("hk_","")
+    file_name.gsub!("hk_","")
     # Check if starts with "20"
     if(file_name[0..1]!="20")then
+      @logger.warn("extract_yyyymm() returning 999999 for #{file_name}")
       return "999999"
     end
     # Extract the first 6 chars
