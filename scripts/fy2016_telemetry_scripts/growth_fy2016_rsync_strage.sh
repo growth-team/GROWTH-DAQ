c82b0b9bb828b1b3df80bb2102a79427266a073e
Yuuki Wada <yuphys.expressif@gmail.com>
2016-10-24 19:47:10 +0900

    modify growth_fy2016_rsync_strage.sh

--- scripts/fy2016_telemetry_scripts/growth_fy2016_rsync_strage.sh
+++ scripts/fy2016_telemetry_scripts/growth_fy2016_rsync_strage.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-=======
 umount /dev/sda1
 sleep 5s
 mount /dev/sda1 /media/pi/Transcend
 sleep 5s
->>>>>>> e05a20ace979e1fcaaea3fd7851560b4e6f0d349
 rsync -avr /home/pi/work/growth /media/pi/Transcend/
