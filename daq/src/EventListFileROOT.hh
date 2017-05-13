daba239e2f6224652f1579e41fe79d3b294ba1b0
Takayuki Yuasa <yuasatakayuki@gmail.com>
2017-05-13 18:33:38 +0800

    EventListFileROOT.hh - event filling part fixed, event tree written to file upon close

--- daq/src/EventListFileROOT.hh
+++ daq/src/EventListFileROOT.hh
@@ -32,10 +32,7 @@ public:
 
 public:
 	~EventListFileROOT() {
-		/*
-		if (outputFile != NULL) {
-			outputFile->Close();
-		}*/
+		close();
 	}
 
 private:
@@ -44,29 +41,26 @@ private:
 		outputFile = new TFile(fileName.c_str(), "recreate");
 		eventTree = new TTree("eventTree", "eventTree");
 
-//    - C : a character string terminated by the 0 character
-//     - B : an 8 bit signed integer (Char_t)
-//     - b : an 8 bit unsigned integer (UChar_t)
-//     - S : a 16 bit signed integer (Short_t)
-//     - s : a 16 bit unsigned integer (UShort_t)
-//     - I : a 32 bit signed integer (Int_t)
-//     - i : a 32 bit unsigned integer (UInt_t)
-//     - F : a 32 bit floating point (Float_t)
-//     - D : a 64 bit floating point (Double_t)
-//     - L : a 64 bit signed integer (Long64_t)
-//     - l : a 64 bit unsigned integer (ULong64_t)
-//     - O : [the letter 'o', not a zero] a boolean (Bool_t)
+		// - C : a character string terminated by the 0 character
+		// - B : an 8 bit signed integer (Char_t)
+		// - b : an 8 bit unsigned integer (UChar_t)
+		// - S : a 16 bit signed integer (Short_t)
+		// - s : a 16 bit unsigned integer (UShort_t)
+		// - I : a 32 bit signed integer (Int_t)
+		// - i : a 32 bit unsigned integer (UInt_t)
+		// - F : a 32 bit floating point (Float_t)
+		// - D : a 64 bit floating point (Double_t)
+		// - L : a 64 bit signed integer (Long64_t)
+		// - l : a 64 bit unsigned integer (ULong64_t)
+		// - O : [the letter 'o', not a zero] a boolean (Bool_t)
 
 		eventTree->Branch("boardIndexAndChannel", &eventEntry.ch, "boardIndexAndChannel/i");
-		//eventTree->Branch("unixTime", &eventEntry.unixTime, "unixTime/D");
 		eventTree->Branch("timeTag", &eventEntry.timeTag, "timeTag/l");
 		eventTree->Branch("triggerCount", &eventEntry.triggerCount, "triggerCount/i");
 		eventTree->Branch("nSamples", &eventEntry.nSamples, "nSamples/i");
-		//eventTree->Branch("energy", &eventEntry.energy, "energy/F");
 		eventTree->Branch("phaMax", &eventEntry.phaMax, "phaMax/s");
 		eventTree->Branch("waveform", eventEntry.waveform, "waveform[nSamples]/s");
 
-		//write header info
 		writeHeader();
 	}
 
@@ -84,8 +78,8 @@ private:
 public:
 	void fillEvents(std::vector<GROWTH_FY2015_ADC_Type::Event*>& events) {
 		for (auto& event : events) {
-			memcpy(&eventTree, &event, sizeof(GROWTH_FY2015_ADC_Type::Event));
-			//eventTree->Fill();
+			eventEntry = *event;
+			eventTree->Fill();
 		}
 	}
 
@@ -99,10 +93,13 @@ public:
 
 public:
 	void close() {
-		//outputFile->Close();
-		outputFile = NULL;
+		if (outputFile != NULL) {
+			eventTree->Write();
+			outputFile->Close();
+			delete outputFile;
+			outputFile = NULL;
+		}
 	}
-
 };
 
 #endif /* EVENTLISTFILEROOT_HH_ */
