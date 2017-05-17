e9b8e08a0c1e3bd93b25bf23f28caf308c1da782
Takayuki Yuasa <yuasatakayuki@users.noreply.github.com>
2017-05-17 21:13:35 +0800

    growth_daq ROOT version fix (#9)
    * fix for ROOT version
    
    * EventListFileROOT.hh - event filling part fixed, event tree written to file upon close
    
    * EventListFileROOT.hh - fill waveform as well
    
    * EventListFileROOT.hh - TTree data type fix

--- daq/src/EventListFileROOT.hh
+++ daq/src/EventListFileROOT.hh
@@ -17,60 +17,91 @@
 #include "TGraph.h"
 
 class EventListFileROOT: public EventListFile {
-private:
-	TTree* eventTree;
-	TFile* outputFile = NULL;
-	std::string detectorID;
-	SpaceFibreADC::Event eventEntry;
-	std::string configurationYAMLFile;
-
 public:
 	EventListFileROOT(std::string fileName, std::string detectorID = "empty", std::string configurationYAMLFile = "") :
 			EventListFile(fileName), detectorID(detectorID), configurationYAMLFile(configurationYAMLFile) {
+		eventEntry.waveform = new uint16_t[SpaceFibreADC::MaxWaveformLength];
 		createOutputRootFile();
 	}
 
-public:
 	~EventListFileROOT() {
-		/*
+		close();
+		delete eventEntry.waveform;
+	}
+
+	void fillEvents(std::vector<GROWTH_FY2015_ADC_Type::Event*>& events) {
+		unixTime = CxxUtilities::Time::getUNIXTimeAsUInt32();
+		for (auto& event : events) {
+			copyEventData(event, &eventEntry);
+			eventTree->Fill();
+		}
+	}
+
+	size_t getEntries() {
+		return eventTree->GetEntries();
+	}
+
+	void fillGPSTime(uint8_t* gpsTimeRegisterBuffer) {}
+
+	void close() {
 		if (outputFile != NULL) {
+			eventTree->Write();
 			outputFile->Close();
-		}*/
+			delete outputFile;
+			outputFile = NULL;
+		}
 	}
 
 private:
 	void createOutputRootFile() {
-		using namespace std;
 		outputFile = new TFile(fileName.c_str(), "recreate");
 		eventTree = new TTree("eventTree", "eventTree");
 
-//    - C : a character string terminated by the 0 character
-//     - B : an 8 bit signed integer (Char_t)
-//     - b : an 8 bit unsigned integer (UChar_t)
-//     - S : a 16 bit signed integer (Short_t)
-//     - s : a 16 bit unsigned integer (UShort_t)
-//     - I : a 32 bit signed integer (Int_t)
-//     - i : a 32 bit unsigned integer (UInt_t)
-//     - F : a 32 bit floating point (Float_t)
-//     - D : a 64 bit floating point (Double_t)
-//     - L : a 64 bit signed integer (Long64_t)
-//     - l : a 64 bit unsigned integer (ULong64_t)
-//     - O : [the letter 'o', not a zero] a boolean (Bool_t)
+		// - C : a character string terminated by the 0 character
+		// - B : an 8 bit signed integer (Char_t)
+		// - b : an 8 bit unsigned integer (UChar_t)
+		// - S : a 16 bit signed integer (Short_t)
+		// - s : a 16 bit unsigned integer (UShort_t)
+		// - I : a 32 bit signed integer (Int_t)
+		// - i : a 32 bit unsigned integer (UInt_t)
+		// - F : a 32 bit floating point (Float_t)
+		// - D : a 64 bit floating point (Double_t)
+		// - L : a 64 bit signed integer (Long64_t)
+		// - l : a 64 bit unsigned integer (ULong64_t)
+		// - O : [the letter 'o', not a zero] a boolean (Bool_t)
 
-		eventTree->Branch("boardIndexAndChannel", &eventEntry.ch, "boardIndexAndChannel/i");
-		//eventTree->Branch("unixTime", &eventEntry.unixTime, "unixTime/D");
+		eventTree->Branch("boardIndexAndChannel", &eventEntry.ch, "boardIndexAndChannel/b");
 		eventTree->Branch("timeTag", &eventEntry.timeTag, "timeTag/l");
-		eventTree->Branch("triggerCount", &eventEntry.triggerCount, "triggerCount/i");
-		eventTree->Branch("nSamples", &eventEntry.nSamples, "nSamples/i");
-		//eventTree->Branch("energy", &eventEntry.energy, "energy/F");
+		eventTree->Branch("unixTime", &unixTime, "unixTime/i");
+		eventTree->Branch("triggerCount", &eventEntry.triggerCount, "triggerCount/s");
+		eventTree->Branch("nSamples", &eventEntry.nSamples, "nSamples/s");
 		eventTree->Branch("phaMax", &eventEntry.phaMax, "phaMax/s");
+		eventTree->Branch("phaMaxTime", &eventEntry.phaMax, "phaMaxTime/s");
+		eventTree->Branch("phaMin", &eventEntry.phaMax, "phaMin/s");
+		eventTree->Branch("phaFirst", &eventEntry.phaMax, "phaFirst/s");
+		eventTree->Branch("phaLast", &eventEntry.phaMax, "phaLast/s");
+		eventTree->Branch("maxDerivative", &eventEntry.phaMax, "maxDerivative/s");
+		eventTree->Branch("baseline", &eventEntry.phaMax, "baseline/s");
 		eventTree->Branch("waveform", eventEntry.waveform, "waveform[nSamples]/s");
 
-		//write header info
 		writeHeader();
 	}
 
-private:
+	void copyEventData(GROWTH_FY2015_ADC_Type::Event* from, GROWTH_FY2015_ADC_Type::Event* to) {
+		to->ch = from->ch;
+		to->timeTag = from->timeTag;
+		to->triggerCount = from->triggerCount;
+		to->phaMax = from->phaMax;
+		to->phaMaxTime = from->phaMaxTime;
+		to->phaMin = from->phaMin;
+		to->phaFirst = from->phaFirst;
+		to->phaLast = from->phaLast;
+		to->maxDerivative = from->maxDerivative;
+		to->baseline = from->baseline;
+		to->nSamples = from->nSamples;
+		memcpy(to->waveform, from->waveform, sizeof(uint16_t)*from->nSamples);
+	}
+
 	void writeHeader(){
 		CxxUtilities::ROOTUtilities::writeObjString("fileCreationDate",
 				CxxUtilities::Time::getCurrentTimeYYYYMMDD_HHMMSS());
@@ -81,28 +112,13 @@ private:
 		}
 	}
 
-public:
-	void fillEvents(std::vector<GROWTH_FY2015_ADC_Type::Event*>& events) {
-		for (auto& event : events) {
-			memcpy(&eventTree, &event, sizeof(GROWTH_FY2015_ADC_Type::Event));
-			//eventTree->Fill();
-		}
-	}
-
-public:
-	size_t getEntries() {
-		return eventTree->GetEntries();
-	}
-
-public:
-	void fillGPSTime(uint8_t* gpsTimeRegisterBuffer) {}
-
-public:
-	void close() {
-		//outputFile->Close();
-		outputFile = NULL;
-	}
-
+private:
+	TTree* eventTree = nullptr;
+	TFile* outputFile = nullptr;
+	std::string detectorID;
+	uint32_t unixTime;
+	GROWTH_FY2015_ADC_Type::Event eventEntry;
+	std::string configurationYAMLFile;
 };
 
 #endif /* EVENTLISTFILEROOT_HH_ */
