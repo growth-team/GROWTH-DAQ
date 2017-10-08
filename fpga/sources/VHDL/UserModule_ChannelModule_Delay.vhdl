71728ae5a28277841b531e9058a8726e7337446d
Takayuki Yuasa <yuasatakayuki@gmail.com>
2017-10-08 12:27:10 +0800

    delay module simplified

--- fpga/sources/VHDL/UserModule_ChannelModule_Delay.vhdl
+++ fpga/sources/VHDL/UserModule_ChannelModule_Delay.vhdl
@@ -1,15 +1,6 @@
---UserModule_ChModule_Delay.vhdl
---
---SpaceWire Board / User FPGA / Modularized Structure Template
---UserModule / Ch Module / Delay Module
---
---ver20071022 Takayuki Yuasa
---file created
---based on UserModule_Template_with_BusIFLite.vhdl (ver20071021)
+-- UserModule_ChModule_Delay.vhdl
+-- Delay incoming ADC data
 
----------------------------------------------------
---Declarations of Libraries
----------------------------------------------------
 library ieee, work;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
@@ -18,9 +9,6 @@ use work.iBus_Library.all;
 use work.iBus_AddressMap.all;
 use work.UserModule_Library.all;
 
----------------------------------------------------
---Entity Declaration
----------------------------------------------------
 entity UserModule_ChModule_Delay is
   port(
     ChModule2InternalModule : in  Signal_ChModule2InternalModule;
@@ -33,27 +21,8 @@ entity UserModule_ChModule_Delay is
     );
 end UserModule_ChModule_Delay;
 
----------------------------------------------------
---Behavioral description
----------------------------------------------------
 architecture Behavioral of UserModule_ChModule_Delay is
 
-  ---------------------------------------------------
-  --Declarations of Components
-  ---------------------------------------------------
-  component UserModule_ChModule_Delay_1Clk_Delay
-    port(
-      AdcDataIn   : in  std_logic_vector(ADCResolution-1 downto 0);
-      AdcDataOut  : out std_logic_vector(ADCResolution-1 downto 0);
-      --clock and reset
-      Clock       : in  std_logic;
-      GlobalReset : in  std_logic
-      );
-  end component;
-
-  ---------------------------------------------------
-  --Declarations of Signals
-  ---------------------------------------------------
   --Signals
   signal Trigger        : std_logic                         := '0';
   signal a              : integer range 0 to MaximumOfDelay := 0;
@@ -65,39 +34,16 @@ architecture Behavioral of UserModule_ChModule_Delay is
   type   AdcDataVector is array (integer range <>) of std_logic_vector(ADCResolution-1 downto 0);
   signal AdcDataArray : AdcDataVector(MaximumOfDelay downto 0);
 
-  ---------------------------------------------------
-  --Beginning of behavioral description
-  ---------------------------------------------------
 begin
 
-  ---------------------------------------------------
-  --Instantiations of Components
-  ---------------------------------------------------
-  Delay : for I in 0 to MaximumOfDelay-1 generate
-    intst_1Clk_Delays : UserModule_ChModule_Delay_1Clk_Delay
-      port map(
-        ADCDataIn   => AdcDataArray(I),
-        ADCDataOut  => AdcDataArray(I+1),
-        Clock       => Clock,
-        GlobalReset => GlobalReset
-        );
-  end generate;
-
-  ---------------------------------------------------
-  --Static relationships
-  ---------------------------------------------------
-  AdcDataArray(0) <= AdcDataIn;
-
-  ---------------------------------------------------
-  --Dynamic Processes with Sensitivity List
-  ---------------------------------------------------
-  --UserModule main state machine
   MainProcess : process (Clock, GlobalReset)
   begin
-    --is this process invoked with GlobalReset?
     if (GlobalReset = '0') then
-      --is this process invoked with Clock Event?
     elsif (Clock'event and Clock = '1') then
+      -- Shift register
+      AdcDataArray <= AdcDataArray(MaximumOfDelay downto 1) & AdcDataIn;
+
+      -- Select output
       if (a < MaximumOfDelay) then
         AdcDataOut <= AdcDataArray(a);
       end if;
@@ -107,75 +53,5 @@ begin
         a <= 0;
       end if;
     end if;
-  end process;
-  
-end Behavioral;
-
----------------------------------------------------
---Declarations of Libraries
----------------------------------------------------
-library ieee, work;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
-use work.iBus_Library.all;
-use work.iBus_AddressMap.all;
-use work.UserModule_Library.all;
-
-
----------------------------------------------------
---Entity Declaration
----------------------------------------------------
-entity UserModule_ChModule_Delay_1Clk_Delay is
-  port(
-    AdcDataIn   : in  std_logic_vector(ADCResolution-1 downto 0);
-    AdcDataOut  : out std_logic_vector(ADCResolution-1 downto 0);
-    --clock and reset
-    Clock       : in  std_logic;
-    GlobalReset : in  std_logic
-    );
-end UserModule_ChModule_Delay_1Clk_Delay;
-
----------------------------------------------------
---Behavioral description
----------------------------------------------------
-architecture Behavioral of UserModule_ChModule_Delay_1Clk_Delay is
-
-  ---------------------------------------------------
-  --Declarations of Components
-  ---------------------------------------------------
-
-  ---------------------------------------------------
-  --Declarations of Signals
-  ---------------------------------------------------
-  signal AdcData : std_logic_vector(ADCResolution-1 downto 0);
-
-  ---------------------------------------------------
-  --Beginning of behavioral description
-  ---------------------------------------------------
-begin
-
-  ---------------------------------------------------
-  --Instantiations of Components
-  ---------------------------------------------------
-
-  ---------------------------------------------------
-  --Static relationships
-  ---------------------------------------------------
-  AdcDataOut <= AdcData;
-
-  ---------------------------------------------------
-  --Dynamic Processes with Sensitivity List
-  ---------------------------------------------------
-  --UserModule main state machine
-  MainProcess : process (Clock, GlobalReset)
-  begin
-    --is this process invoked with GlobalReset?
-    if (GlobalReset = '0') then
-      --is this process invoked with Clock Event?
-    elsif (Clock'event and Clock = '1') then
-      AdcData <= AdcDataIn;
-    end if;
-  end process;
-  
+  end process;  
 end Behavioral;
