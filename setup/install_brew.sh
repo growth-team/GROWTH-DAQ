10481ad6e2685c8e39804ad32e69a7799d68bf11
Takayuki Yuasa <yuasatakayuki@users.noreply.github.com>
2017-10-24 08:55:48 +0800

    installs git-lfs (#17)

--- setup/install_brew.sh
+++ setup/install_brew.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 brew install cmake
 brew install yaml-cpp
 brew install cfitsio
@@ -8,3 +10,7 @@ brew install xerces-c
 brew install zmq
 brew tap IceCube-SPNO/icecube
 brew install cppzmq
+brew install git-lfs
+
+# Run git-lfs install command
+git lfs install
