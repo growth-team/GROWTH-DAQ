8b718190b6c9d7128ff4725f41f2d1164805a4ab
YuukiWada <yuphys.expressif@gmail.com>
2017-11-02 18:05:28 +0900

    modified setup.sh

--- scripts/fy2017_scripts/setup.sh
+++ scripts/fy2017_scripts/setup.sh
@@ -5,28 +5,17 @@ HOMEDIR=/home/pi
 cp -r ${HOMEDIR}/git/GROWTH-DAQ/scripts/fy2017_scripts/scripts ${HOMEDIR}
 cd ${HOMEDIR}/scripts/stand_alone_system
 mv fits_backup.sh_sample fits_backup.sh
-<<<<<<< HEAD
 mv auto_portforward.sh/sample auto_portforward.sh
 sed -i -e "s/growth-fy2017z/${detectorID}/g" fits_backup.sh
 sed -i -e "s/xxxx/${portNumber}/g" auto_portforward.sh
 cd ${HOMEDIR}
 cp ${HOMEDIR}/git/GROWTH-DAQ/scripts/fy2017_scripts/growth_config.yaml ${HOMEDIR}/
-=======
-mv auto_portforward.sh_sample auto_portforward.sh
-sed -i -e "s/growth-fy2017z/${detectorID}/g" fits_backup.sh
-sed -i -e "s/xxxx/${portNumber}/g" auto_portforward.sh
-cd ${HOMEDIR}
->>>>>>> 83c7ca02a6df0f71262670b6948be899054cfb1a
 sed -i -e "s/growth-fy2017z/${detectorID}/g" growth_config.yaml
 cd ${HOMEDIR}/git/GROWTH-DAQ/daq/configurationFile
 cp configuration_growth-fy2017z.yaml configuration_${detectorID}.yaml
 sed -i -e "s/growth-fy2017z/${detectorID}/g" configuration_${detectorID}.yaml
 rm ${HOMEDIR}/bootcount.text
 rm -rf ${HOMEDIR}/work/growth/data/growth-fy2017z
-<<<<<<< HEAD
 rm -rf /var/log/growth/*
-=======
-rm -rf /var/log/*
->>>>>>> 83c7ca02a6df0f71262670b6948be899054cfb1a
 
 echo "Modify crontab"
