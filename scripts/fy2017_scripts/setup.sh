a7f6844fa3aa48367644eeeb683daabaff987204
YuukiWada <yuphys.expressif@gmail.com>
2017-11-02 18:06:59 +0900

    modified setup.sh

--- scripts/fy2017_scripts/setup.sh
+++ scripts/fy2017_scripts/setup.sh
@@ -1,11 +1,11 @@
-detectorID=growth-fy2016a
-portNumber=2101
+detectorID=growth-fy2016g
+portNumber=2107
 HOMEDIR=/home/pi
 
 cp -r ${HOMEDIR}/git/GROWTH-DAQ/scripts/fy2017_scripts/scripts ${HOMEDIR}
 cd ${HOMEDIR}/scripts/stand_alone_system
 mv fits_backup.sh_sample fits_backup.sh
-mv auto_portforward.sh/sample auto_portforward.sh
+mv auto_portforward.sh_sample auto_portforward.sh
 sed -i -e "s/growth-fy2017z/${detectorID}/g" fits_backup.sh
 sed -i -e "s/xxxx/${portNumber}/g" auto_portforward.sh
 cd ${HOMEDIR}
