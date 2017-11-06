e7fe24652f62ee399b4f2fb7821dd3d5ea046712
YuukiWada <yuphys.expressif@gmail.com>
2017-11-06 11:58:39 +0900

    modified setup.sh

--- scripts/fy2017_scripts/setup.sh
+++ scripts/fy2017_scripts/setup.sh
@@ -6,14 +6,14 @@ cp -r ${HOMEDIR}/git/GROWTH-DAQ/scripts/fy2017_scripts/scripts ${HOMEDIR}
 cd ${HOMEDIR}/scripts/stand_alone_system
 mv fits_backup.sh_sample fits_backup.sh
 mv auto_portforward.sh_sample auto_portforward.sh
-sed -i -e "s/growth-fy2017z/${detectorID}/g" fits_backup.sh
+sed -i -e "s/growth-fy2017/${detectorID}/g" fits_backup.sh
 sed -i -e "s/xxxx/${portNumber}/g" auto_portforward.sh
 cd ${HOMEDIR}
 cp ${HOMEDIR}/git/GROWTH-DAQ/scripts/fy2017_scripts/growth_config.yaml ${HOMEDIR}/
-sed -i -e "s/growth-fy2017z/${detectorID}/g" growth_config.yaml
+sed -i -e "s/growth-fy2017/${detectorID}/g" growth_config.yaml
 cd ${HOMEDIR}/git/GROWTH-DAQ/daq/configurationFile
-cp configuration_growth-fy2017z.yaml configuration_${detectorID}.yaml
-sed -i -e "s/growth-fy2017z/${detectorID}/g" configuration_${detectorID}.yaml
+cp configuration_growth-fy2017_triggerMode.yaml configuration_${detectorID}.yaml
+sed -i -e "s/growth-fy2017/${detectorID}/g" configuration_${detectorID}.yaml
 rm ${HOMEDIR}/bootcount.text
 rm -rf ${HOMEDIR}/work/growth/data/growth-fy2017z
 rm -rf /var/log/growth/*
