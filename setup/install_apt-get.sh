75ce67f15701ac45729c85e6843ce863f4027a0a
Takayuki Yuasa <yuasatakayuki@gmail.com>
2019-09-30 23:25:14 +0800

    install_apt-get.sh - remove obsolete packages

--- setup/install_apt-get.sh
+++ setup/install_apt-get.sh
@@ -2,51 +2,49 @@
 
 set -o errexit
 
-apt-get install -y software-properties-common
-apt-get update
-sudo apt-get install -y automake
-apt-get install -y binutils
-apt-get install -y build-essential
-apt-get install -y chromium
-apt-get install -y cmake
-apt-get install -y curl
-apt-get install -y curl-dev
-apt-get install -y dpkg-dev
-apt-get install -y fswebcam
-apt-get install -y git
-apt-get install -y i2c-tools
-apt-get install -y imagemagick
-apt-get install -y libboost1.50-all
-apt-get install -y libbz2-dev
-apt-get install -y libcfitsio3-dev
-apt-get install -y libcurl4-openssl-dev
-apt-get install -y libexpat-dev
-apt-get install -y libjpeg8-dev
-apt-get install -y libncurses-dev
-apt-get install -y libssl-dev
-apt-get install -y libtool-bin
-apt-get install -y libx11-dev
-apt-get install -y libxerces-c-dev
-apt-get install -y libxext-dev
-apt-get install -y libxft-dev
-apt-get install -y libxpm-dev
-apt-get install -y libyaml-cpp-dev
-apt-get install -y libzmq-dev
-apt-get install -y libzmq3-dev
-apt-get install -y make
-apt-get install -y python-dev
-apt-get install -y python-imaging
-apt-get install -y python-smbus
-apt-get install -y python-zmq
-apt-get install -y rails
-apt-get install -y ruby
-apt-get install -y ruby-dev
-apt-get install -y ruby-ffi
-apt-get install -y subversion
-apt-get install -y supervisor
-apt-get install -y swig
-apt-get install -y texinfo
-apt-get install -y wget
-apt-get install -y wicd-curses
-apt-get install -y zlib1g-dev
-apt-get install -y zsh
+sudo apt-get install -y software-properties-common
+sudo apt-get update
+sudo apt-get install -y \
+  automake \
+  binutils \
+  build-essential \
+  chromium \
+  cmake \
+  curl \
+  dpkg-dev \
+  fswebcam \
+  git \
+  i2c-tools \
+  imagemagick \
+  libboost-all-dev \
+  libbz2-dev \
+  libcfitsio-dev \
+  libcurl4-openssl-dev \
+  libexpat-dev \
+  libjpeg8-dev \
+  libncurses-dev \
+  libssl-dev \
+  libtool-bin \
+  libx11-dev \
+  libxerces-c-dev \
+  libxext-dev \
+  libxft-dev \
+  libxpm-dev \
+  libyaml-cpp-dev \
+  libzmq3-dev \
+  make \
+  python-dev \
+  python-smbus \
+  python-zmq \
+  rails \
+  ruby \
+  ruby-dev \
+  ruby-ffi \
+  subversion \
+  supervisor \
+  swig \
+  texinfo \
+  wget \
+  wicd-curses \
+  zlib1g-dev \
+  zsh
